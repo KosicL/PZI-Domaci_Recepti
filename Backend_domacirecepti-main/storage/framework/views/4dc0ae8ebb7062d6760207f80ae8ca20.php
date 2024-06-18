fe9bc8a8bba76f266b6c2b294a5ebe145a276465
KosicL <108084728+KosicL@users.noreply.github.com>
2024-06-18 18:21:06 +0200

    Add files via upload

---
+++ Backend_domacirecepti-main/storage/framework/views/4dc0ae8ebb7062d6760207f80ae8ca20.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Dobrodošli!</h1>
+    <p>Vaš kod za potvrdu je: <?php echo e($confirmationCode); ?></p>
+</body>
+</html>
+<?php /**PATH D:\Fakultet\Programiranje za Internet\Backend_domacirecepti-main\resources\views/emails/confirmationCode.blade.php ENDPATH**/ ?>
